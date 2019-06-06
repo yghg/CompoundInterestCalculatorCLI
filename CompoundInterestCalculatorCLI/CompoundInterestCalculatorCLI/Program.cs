56dbd446272564175b959a00318129cc45c438a7
yghg <43450199+yghg@users.noreply.github.com>
2019-06-06 12:45:58 -0400

    Add files via upload

---
+++ CompoundInterestCalculatorCLI/CompoundInterestCalculatorCLI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundInterestCalculatorCLI
+{
+    class Program
+    {
+        // Balance(Y)   =   P(1 + r)Y   +   c[ ((1 + r)Y + 1 - (1 + r)) / r ]
+
+        private double total;
+        private string principal;
+        private string interest;
+        private string years;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.GetValues();
+            p.total = p.CalculateInterest();
+
+            Console.WriteLine("Future Value: " + p.total.ToString("C2"));
+            Console.ReadLine();
+        }
+
+        private void GetValues()
+        {
+            Console.Write("Principal: ");
+            principal = Console.ReadLine();
+
+            Console.Write("Interest Rate (As Decimal): ");
+            interest = Console.ReadLine();
+
+            Console.Write("Years to Compound: ");
+            years = Console.ReadLine();
+        }
+
+        private double CalculateInterest()
+        {
+            total = double.Parse(principal) * Math.Pow((1 + (double.Parse(interest) / 1)), (1 * int.Parse(years)));
+            return total;
+        }
+    }
+}
---
+++ CompoundInterestCalculatorCLI/CompoundInterestCalculatorCLI/Program_v2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundInterestCalculatorCLI
+{
+    class Program
+    {
+        // variables
+        private double compound;
+        private string txtPrincipal;
+        private double principal;
+        private string txtInterest;
+        private double interest;
+        private string txtYears;
+        private int years;
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("SIMPLE COMPOUND INTEREST CALCULATOR");
+            Console.WriteLine("");
+
+            Program p = new Program();
+            p.GetValues();
+            p.compound = p.CalculateInterest();
+
+            Console.WriteLine("");
+            Console.WriteLine("Future Value: " + p.compound.ToString("C2"));
+            Console.WriteLine("");
+            Console.WriteLine("(PRESS ANY KEY TO EXIT)");
+            Console.ReadLine();
+        }
+
+        // get and validate input from user
+        private void GetValues()
+        {
+            try
+            {
+                Console.Write("Principal: ");
+                txtPrincipal = Console.ReadLine();
+                bool isDoubleP = Double.TryParse(txtPrincipal, out principal);
+                while (!isDoubleP)
+                {
+                    Console.Write("Principal: ");
+                    txtPrincipal = Console.ReadLine();
+                    isDoubleP = Double.TryParse(txtPrincipal, out principal);
+                    continue;
+                }
+                principal = double.Parse(txtPrincipal);
+
+                Console.Write("Interest Rate (As Decimal): ");
+                txtInterest = Console.ReadLine();
+                bool isDoubleI = Double.TryParse(txtInterest, out interest);
+                while (!isDoubleI)
+                {
+                    Console.Write("Interest Rate (As Decimal): ");
+                    txtInterest = Console.ReadLine();
+                    isDoubleI = Double.TryParse(txtInterest, out interest);
+                    continue;
+                }
+                interest = double.Parse(txtInterest);
+
+                Console.Write("Years to Compound: ");
+                txtYears = Console.ReadLine();
+                bool isDoubleY = int.TryParse(txtYears, out years);
+                while (!isDoubleY)
+                {
+                    Console.Write("Years to Compound: ");
+                    txtYears = Console.ReadLine();
+                    isDoubleY = int.TryParse(txtYears, out years);
+                    continue;
+                }
+                years = int.Parse(txtYears);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                throw;
+            }
+            
+        }
+
+        // do the math
+        // compound = principal(1 + (interest/n)^(n*years))
+        private double CalculateInterest()
+        {
+            try
+            {
+                compound = (principal * Math.Pow((1 + (interest / 1)), (1 * years)));
+                return compound;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                throw;
+            }            
+        }
+    }
+}
